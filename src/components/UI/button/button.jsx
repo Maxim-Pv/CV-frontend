11934c98d9ab1c32a95d57be6ba2ce4dda26b07c
Maksim Pavlov <maxim.paw2010@yandex.ru>
2023-10-12 14:46:28 +0300

    add homework 1

---
+++ src/components/UI/button/button.jsx
@@ -0,0 +1,9 @@
+import './button.css';
+
+const Button = function () {
+    return (
+        <button className='button'>Advanced Search</button>
+    )
+}
+
+export default Button;
---
+++ src/components/UI/input/input.jsx
@@ -0,0 +1,8 @@
+import './input.css';
+
+const Input = function () {
+    return <input className='search' placeholder='Search' /> 
+}
+
+export default Input;
+
